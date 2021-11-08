02388524b6841b1c724f5672177d96bbd89bfa95
savannah-martin <78399457+savannah-martin@users.noreply.github.com>
2021-11-07 19:39:11 -0600

    class

--- app.js
+++ app.js
@@ -1,39 +1,72 @@
-Vue.createApp({
-    data() {
-      return {
-        allPokemon: [
-            {id: 1, name:'bulbasaur', type: 'grass'},
-            {id: 2, name: 'ivysaur', type: 'grass'}
-        ],
-        partyPokemon: [
-            {id: 2, name: 'ivysaur', type: 'grass'},
-        ],
-        filteredPokemon: [
-            {id: 2, name: 'ivysaur', type: 'grass'},
-        ],
-        maxPartySize: 6
+const getPokemon = async function (id) {
+    // get pokemon data from pokeapi
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  
+    const response = await fetch(url);
+    const data = await response.json();
+  
+    return data;
+    //createPokemonCard( data );
+  };
+  
+
+const app = Vue.createApp({
+  data() {
+    return {
+      allPokemon: [
+      ],
+      partyPokemon: [],
+      filteredPokemon: [],
+      maxPartySize: 6,
+    };
+  },
+  methods: {
+    async loadPokemon() {
+      // load all pokemon from API and save into all pokemon
+      const pokemon_count = 20;
+      let pokemon = [];
+      for (let i = 1; i <= pokemon_count; i++) {
+        let p = await getPokemon(i);
+        if (p.name === "mr-mime") {
+          p.name = "Mr. Mime";
+        }
+        p.isFavorite = false;
+
+        pokemon.push(p);
+      }
+
+      pokemon.forEach( p => {
+          this.allPokemon.push(p);
+      });
+    },
+    filterPokemon() {
+      // set filteredPokemon to matching pokemon based on search query
+    },
+    addPokemonToParty(pokemon) 
+    {
+      const pokemonCopy = {...pokemon};
+      pokemonCopy.guid = this.getGUID();
+      console.log(pokemonCopy.guid);
+      this.partyPokemon.push(  pokemonCopy );
+    },
+    removePokemonFromParty(pokemon) {
+      
+      this.partyPokemon = this.partyPokemon
+                            .filter( p => p.guid != pokemon.guid);
+    },
+    pokemonTypeString(pokemon) {
+      if (pokemon.types.length > 1) {
+        return `${pokemon.types[0].type.name} / ${pokemon.types[1].type.name}`;
+      } else {
+        return `${pokemon.types[0].type.name}`;
       }
     },
-
-    methods: {
-        loadPokemon() {
-
-        },
-        filterPokemon() {
-
-        },
-        addPokemonToParty(pokemon) {
-
-        },
-        removePokemonFromParty(pokemon) {
-
-        },
-        pokemonTypeString(pokemon) {
-            if (pokemon.types.length > 1){
-                return `${pokemon.types.type.name}`;
-            }
-        }
+    getGUID() {
+      return Math.floor(Math.random()* 1000000);
     }
-
-  }).mount('#app')
+  },
+  mounted() {
+    this.loadPokemon();
+  },
+}).mount("#app");
 
