affda13451cb380fb2ec36802fadc008cf8d153a
savannah-martin <78399457+savannah-martin@users.noreply.github.com>
2021-11-14 18:27:19 -0600

    /

--- app.js
+++ app.js
@@ -10,6 +10,26 @@ const getPokemon = async function (id) {
   //createPokemonCard( data );
 };
 
+const colors = {
+  fire: '#fd7d24',
+  grass: '#9bcc50',
+  electric: '#eed535',
+  water: '#4592c4',
+  ground: '#ab9842',
+  rock: '#a38c21',
+  fairy: '#fdb9e9',
+  poison: '#b97fc9',
+  bug: '#729f3f',
+  dragon: '#7038f8',
+  psychic: '#f366b9',
+  flying: '#3dc7ef',
+  fighting: '#d56723',
+  normal: '#a4acaf',
+  ice: '#51c4e7',
+  ghost: '#7b62a3',
+  dark: '#707070',
+  steel: '#9eb7b8'
+};
 
 const app = Vue.createApp({
   data() {
